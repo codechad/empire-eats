e97575ec13319901561e751fb9091922b535a83b
Chad <andchadtx@gmail.com>
2019-10-03 16:14:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,41 @@
 /* Setting up the variable for the service worker functions */
 const staticCacheName = 'restaurant-reviews-v4';
 
-/* Installs the data to store */
-self.addEventListener('install', function (event) {
-    // console.log("Service Worker installed");
-    event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            // console.log("Service Worker installed");
             return cache.addAll([
-                './',
-                './index.html',
-                './restaurant.html',
-                './data/restaurants.json',
-                './sw.js',
-                './favicon-32x32.png',
-                './css/styles.css',
-                './img/1.jpg',
-                './img/10.jpg',
-                './img/2.jpg',
-                './img/3.jpg',
-                './img/4.jpg',
-                './img/5.jpg',
-                './img/6.jpg',
-                './img/7.jpg',
-                './img/8.jpg',
-                './img/9.jpg',
-                './img/marker-icon-2x-red.png',
-                './img/marker-shadow.png',
-                './js/idb.js',
-                './js/dbhelper.js',
-                './js/bouncemarker.js',
-                './js/main.js',
-                './js/restaurant_info.js',
+                '/',
+                'css/styles.css',
+                'data/restaurants.json',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
+                'img/favicon-32x32.png',
+                'js/dbhelper.js',
+                'js/main.js',
+                'js/restaurant_info.js',
                 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
                 'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+                'restaurant.html'
             ]);
         })
     );
-    // console.log("cache successful");
 });
-
 
 // deletes old cache
 self.addEventListener('activate', function (event) {
             // console.log("Service Worker activated");
-            event.waitUntil(
-                caches.keys().then(function (cacheNames) {
+            e.waitUntil(
+                caches.keys().then(cacheNames => {
                     return Promise.all(
                         cacheNames.filter(function (cacheName) {
                             return cacheName.startsWith(staticCacheName) &&
