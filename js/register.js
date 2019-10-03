990fe62d9249af700f8a5e33b07a72363f7b8706
Chad <andchadtx@gmail.com>
2019-10-03 12:22:28 -0500

    Update register.js

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@
      * Register Service Worker.
      */
     if (navigator.serviceWorker) {
-        navigator.serviceWorker.register('./sw.js').then(function (reg) {
+        navigator.serviceWorker.register('sw.js').then(function (reg) {
             console.log("Service Worker Registered", reg);
         }).catch(function (err) {
             console.log("Service Worker failed to register", err);
