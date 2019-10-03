07568312d932bce1c8f17175739ec493ac739c08
Chad <andchadtx@gmail.com>
2019-10-03 15:52:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = restaurant.name + " Main Image";
+  image.alt = DBHelper.altForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
