56d065f9bb4974280c5c953004aa6d8d9d060c74
Chad <andchadtx@gmail.com>
2019-10-28 21:43:39 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 initMap = () => {
   self.newMap = L.map('map', {
     center: [40.722216, -73.987501],
-    zoom: 12,
+    zoom: 13,
     scrollWheelZoom: false
   });
 
